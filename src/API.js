33e28ee2497dab0f4f0ae114cbd4f4801f476dfc
Prashant-kumar-pcpk <prashantkumar.pcpk@gmail.com>
2026-07-17 18:46:24 +0530

    update the backend url

--- src/API.js
+++ src/API.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: process.env.NODE_ENV === "production"
     ? "" // production -> same origin
-    : "http://localhost:5060", // development backend port
+    : "https://my-portfolio-be-27dm.onrender.com/", // development backend port
   headers: {
     "Content-Type": "application/json",
   },
